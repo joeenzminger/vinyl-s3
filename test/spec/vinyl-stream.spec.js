1562bae2f649977252a7f6a52005cdadb6da69e5
hmalphettes <hmalphettes@gmail.com>
2016-11-02 17:41:29 +0800

    Upgrade the dependencies, remove lodash and through2-map

--- test/spec/vinyl-stream.spec.js
+++ test/spec/vinyl-stream.spec.js
@@ -7,43 +7,43 @@ var createVinylStream = require('vinyl-stream'),
 describe('#createVinylStream', function() {
 
 	it('should map `Body` to `contents`', function() {
-		var stream = createVinylStream(), body = new Buffer(24);
+		var stream = createVinylStream(), body = Buffer.alloc(24);
 		stream.write({ Bucket: 'foo', Key: 'key', Body: body });
 		expect(stream.read()).to.include({ contents: body });
 	});
 
 	it('should prefer `base` value provided in options', function() {
-		var stream = createVinylStream({ base: 'bar' }), body = new Buffer(24);
+		var stream = createVinylStream({ base: 'bar' }), body = Buffer.alloc(24);
 		stream.write({ Bucket: 'foo', Key: 'key', Body: body });
 		expect(stream.read()).to.include({ base: 'bar' });
 	});
 
 	it('should prefer `cwd` value provided in options', function() {
-		var stream = createVinylStream({ cwd: 'test' }), body = new Buffer(24);
+		var stream = createVinylStream({ cwd: 'test' }), body = Buffer.alloc(24);
 		stream.write({ Bucket: 'test', Key: 'key', Body: body });
 		expect(stream.read()).to.include({ cwd: 'test' });
 	});
 
 	it('should correctly map `Bucket` -> `base`', function() {
-		var stream = createVinylStream(), body = new Buffer(24);
+		var stream = createVinylStream(), body = Buffer.alloc(24);
 		stream.write({ Bucket: 'test', Key: 'key', Body: body });
 		expect(stream.read()).to.include({ base: 'test' });
 	});
 
 	it('should correctly map `LastModified` -> `fs.Stat` object', function() {
-		var stream = createVinylStream(), date = new Date(), body = new Buffer(24);
+		var stream = createVinylStream(), date = new Date(), body = Buffer.alloc(24);
 		stream.write({ LastModified: date, Key: 'key', Body: body });
 		expect(stream.read().stat.mtime.toString()).to.equal(date.toString());
 	});
 
 	it('should collect metadata when meta is enabled', function() {
-		var stream = createVinylStream({ meta: true }), body = new Buffer(24);
+		var stream = createVinylStream({ meta: true }), body = Buffer.alloc(24);
 		stream.write({ ContentType: 'foo/bar', Key: 'key', Body: body });
 		expect(stream.read()).to.include({ contentType: 'foo/bar' });
 	});
 
 	it('should omit metadata when meta is disabled', function() {
-		var stream = createVinylStream({ meta: false }), body = new Buffer(24);
+		var stream = createVinylStream({ meta: false }), body = Buffer.alloc(24);
 		stream.write({ ContentType: 'foo/bar', Key: 'key', Body: body });
 		expect(stream.read()).to.not.include({ contentType: 'foo/bar' });
 	});
